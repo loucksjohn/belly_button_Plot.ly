fb3cb806d037241d06d04217c2aa006837ba6f82
John Loucks <johnloucks@gmail.com>
2020-11-17 15:19:18 -0600

    adding bubble chart

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ function mycharts(response){
         var sample_values = item.sample_values.slice(0,10).reverse()
         var otu_ids = item.otu_ids.slice(0,10).reverse()
         var otu_labels = item.otu_labels.slice(0,10).reverse()
+        var sample_values2 = item.sample_values.reverse()
+        var otu_ids2 = item.otu_ids.reverse()
+        var otu_labels2 = item.otu_labels.reverse()
         console.log(sample_values);
         console.log(otu_ids);
         console.log(otu_labels);
@@ -42,17 +45,35 @@ function mycharts(response){
             orientation: "h",
             x: sample_values,
             y: otu_ids.map(each => `OTU ${each}`),
-            text: otu_labels
+            text: otu_labels,
         }];
 
-        
-
-        var layout = {
-            title: "belly button bacteria",
-               
+        var trace2 = [{
+            x: otu_ids2,
+            y: sample_values2,
+            text: otu_labels2,
+            mode: 'markers',
+            marker:  {
+                color: otu_ids2,
+                colorscale:  'Viridis',
+                size: sample_values2,
             }
+                
+            }];
+
+        //var chartData = [trace1, trace2];
+
+        var layout1 = {
+            title: "Test Subject's Top 10 OTU's (Abundance of Species)",               
+            }
+
+        var layout2 = {
+            title:  "Full Profile of Test Subject's OTU Spectrum"
+
+        }
         
-        Plotly.newPlot("bar", trace1, layout);
+        Plotly.newPlot("bar", trace1, layout1);
+        Plotly.newPlot("bubble", trace2, layout2);
     });
 
 
