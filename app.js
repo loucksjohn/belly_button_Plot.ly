b3e969c03a9872d5f18e6b9206d7b5efc05e2b10
John Loucks <johnloucks@gmail.com>
2020-11-17 10:55:26 -0600

    first push of working bar chart

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ function demoBox(sample){
 
         var metadata = data.metadata;
         var array = metadata.filter(sampleObject => sampleObject.id == sample);
+        console.log(array)
     
         var arrayItems = array[0];
+        console.log(arrayItems)
 
         var dataBox = d3.select("#sample-metadata");
         dataBox.html("");
@@ -18,13 +20,53 @@ function demoBox(sample){
 
 }
 
-
 // charts start here
-// function mycharts(){}
+function mycharts(response){
+    d3.json("samples.json").then((data) => {
+        var sampleData = data.samples;
+        var array = sampleData.filter(responseObject => responseObject.id == response);
+        console.log(array)
+
+        var item = array[0]
+        console.log(item)
+        var sortedItem = item.sort((a,b) => b.sample_values - a.sample_values);
+        console.log(sortedItems)
+
+        var sample_values = item.sample_values
+        var otu_ids = item.otu_ids
+        var otu_labels = item.otu_labels
+        console.log(sample_values);
+        console.log(otu_ids);
+        console.log(otu_labels);
+
+        var trace1 = {
+            type: "bar",
+            orientation: "h",
+            x: sample_values,
+            y: otu_ids,
+            text: otu_labels
+        };
+
+        var chartData = [trace1]
+
+        var layout = {
+            title: "belly button bacteria",
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100    
+            }
+        };
+        Plotly.newPlot("bar", chartData, layout);
+
+
+    })
+}
 
 
 
-// Dropdown even trigger
+// Dropdown event trigger
 function init(){
     var selectData = d3.select("#selDataset");
 
@@ -35,7 +77,9 @@ function init(){
         });
 
         var newData = names[0];
-        demoBox(newData);   
+        console.log(newData);
+        demoBox(newData);         
+        mycharts(newData);
 
 
     });
